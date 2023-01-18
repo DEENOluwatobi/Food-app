3fd9e90d7fcac322ff2a73588771dd12ba0b49ea
DEENOluwatobi <shodimutobie@gmail.com>
2023-01-19 00:43:19 +0100

    Admin panel creation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { MdShoppingBasket } from 'react-icons/md'
+import React, { useState } from 'react'
+import { MdShoppingBasket, MdAdd, MdLogout } from 'react-icons/md'
 import { motion } from 'framer-motion'
 import { Link } from 'react-router-dom'
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
@@ -18,13 +18,18 @@ const Header = () => {
 
     const [{user}, dispatch] = useStateValue();
 
+    const  [isMenu, setIsMenu] = useState(false)
+
     const login = async () => {
-        const {user : {refreshToken, providerData}} = await signInWithPopup(firebaseAuth, provider);
-        dispatch({
-            type : actionType.SET_USER,
-            user : providerData[0]
-        })
-        localStorage.setItem('user', JSON.stringify(providerData[0]));
+        if(!user) {
+            const {user : {refreshToken, providerData}} = await signInWithPopup(firebaseAuth, provider);
+            dispatch({
+                type : actionType.SET_USER,
+                user : providerData[0]
+            })
+            localStorage.setItem('user', JSON.stringify(providerData[0]));            
+        }
+
     };
 
   return (
@@ -63,6 +68,24 @@ const Header = () => {
                         alt="userprofile" 
                         onClick={login}
                     />
+                    {
+                    isMenu && (
+                        <div className='w-40 flex bg-gray-50 flex-col shadow-xl rounded-lg absolute top-12 right-0'>
+                        {
+                            user && user.email === 'shodimutobie@gmail.com' && (
+                                <Link to = {'/createItem'}>
+                                    <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 
+                                    transition-all duration-100 ease-in-out
+                                    text-textColor text-base'>New Item <MdAdd/></p>
+                                </Link>
+                            )
+                        }
+                            <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 
+                            transition-all duration-100 ease-in-out
+                            text-textColor text-base'>Logout <MdLogout/></p>
+                        </div>
+                    )
+                    }
                 </div>
             </div>
         </div>
