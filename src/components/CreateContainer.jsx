ab6f986c86105a648f8a9def5b11f5c79ad30acd
DEENOluwatobi <shodimutobie@gmail.com>
2023-01-21 02:03:10 +0100

    completed UI of 'New Items' page

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import { motion } from 'framer-motion';
-import { MdFastfood } from 'react-icons/md';
+import { MdAttachMoney, MdCloudUpload, MdDelete, MdFastfood, MdFoodBank } from 'react-icons/md';
+import { categories } from '../utils/data'
+import Loader from './Loader';
 
 const CreateContainer = () => {
 
@@ -14,10 +16,23 @@ const CreateContainer = () => {
   const [ msg, setMsg] = useState(null);
   const [ isLoading, setIsLoading] = useState(false);
 
+  const uploadImage = () => {
+    
+  };
+
+  const deleteImage = () => {
+    
+  };
+
+  const saveDetails = () => {
+    
+  }
+
+
   return (
-    <div className='flex w-full min-h-screen mt-14 md:mt-20 items-center justify-center'>
+    <div className='flex w-full min-h-screen items-center justify-center'>
       <div className='w-[90%] md:w-[75%] border border-gray-200 rounded-lg p-4
-      flex items-center justify-center'>
+      flex items-center justify-center flex-col gap-4'>
         {fields && (
             <motion.p 
               initial={{opacity : 0}}
@@ -38,10 +53,106 @@ const CreateContainer = () => {
             value={title}
             onChange={(e) => setTitle(e.target.value)} 
             placeholder='Enter title'
-            className='w-full h-full text-lg bg-transparent font-semibold outline-none border-none
+            className='w-full h-full text-lg bg-transparent outline-none border-none
              placeholder:text-gray-500 text-textColor' 
           />
-          
+        </div>
+
+        <div className='w-full'>
+          <select 
+            className='outline-none w-full text-base border-b-2 border-gray-200 p-2 cursor-pointer rounded-md'
+            onChange={(e) => setCategory(e.target.value)}>
+            <option 
+              value="other"
+              className='bg-white'
+            >
+              Select Category
+            </option>
+            {categories && categories.map(item => (
+              <option
+                key={item.id}
+                className='text-base border-0 outline-none capitalize bg-white text-headingColor'
+                value={item.urlParamName}
+              >{item.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div 
+          className='group flex justify-center items-center flex-col border-2 
+          border-dotted border-gray-300 w-full h-225 md:h-450 cursor-pointer rounded-lg'>
+            {isLoading ? <Loader/> : <>
+                {!imageAsset ? (<>
+                  <label className='w-full h-full flex flex-col items-center justify-center'>
+                    <div className='w-full h-full flex flex-col items-center justify-center gap-2'>
+                      <MdCloudUpload className='text-gray-500 text-3xl hover:text-gray-700'/>
+                      <p className='text-gray-500 hover:text-gray-700'>Click here to upload</p>
+                    </div>
+                    <input 
+                      type="file" 
+                      name="uploadimage" 
+                      accept='image/*' 
+                      onChange={uploadImage}
+                      className='w-0 h-0'
+                    />
+                  </label>
+                </>
+                ) : (
+                  <>
+                  <div className='relative h-full'>
+                    <img 
+                      src={imageAsset} 
+                      alt="uploadedimage"
+                      className='w-full h-full object-cover' 
+                    />
+                    <button 
+                      type='button' 
+                      className='absolute bottom-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer
+                        outline-none hover:shadow-md duration-100 transition-all ease-in-out'
+                      onClick={deleteImage}  
+                        ><MdDelete className='text-white'/></button>
+                  </div>
+                  </>
+                )}
+            </>}
+        </div>
+        
+        <div className='w-full flex-col md:flex-row items-center gap-3'>
+          <div className='w-full py-2 border-b border-gray-300 flex items-center gap-2'>
+            <MdFoodBank className='text-gray-700 text-2xl'/>
+            <input 
+              type="text" 
+              placeholder='Calories'
+              required
+              value={calories}
+              onChange={(e) => setCalories(e.target.value)}
+              className='w-full h-full text-lg bg-transparent outline-none border-none
+              placeholder:text-gray-500 text-textColor'
+            />
+          </div>
+
+          <div className='w-full py-2 border-b border-gray-300 flex items-center gap-2'>
+            <MdAttachMoney className='text-gray-700 text-2xl'/>
+            <input 
+              type="text" 
+              placeholder='Price'
+              required
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              className='w-full h-full text-lg bg-transparent outline-none border-none
+              placeholder:text-gray-500 text-textColor'
+            />
+          </div>
+
+        </div>
+        <div className='flex items-center w-full'>
+          <button 
+            type='button'
+            className='ml-0 md:ml-auto w-full md:w-auto border-none outline-none bg-yellow-500 px-12 py-2
+            rounded-lg text-lg font-semibold cursor-pointer hover:shadow-md'
+            onClick={saveDetails}
+          >Save</button>
         </div>
       </div>
       
